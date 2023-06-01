215688ddfefb42821be9257998ce24889e238766
malak <malaklaarabi63@gmail.com>
2023-06-01 06:18:02 +0100

    first commit

--- src/MyTime.js
+++ src/MyTime.js
@@ -3,8 +3,8 @@ import React , {useEffect, useState} from 'react'
 function MyTime() {
   var time = new Date(); 
   var heure = time.getHours(); 
-  var minute = time. getMinutes(); 
-  var seconde = time. getSeconds(); 
+  var minute = time.getMinutes(); 
+  var seconde = time.getSeconds(); 
 
   var formatDeHeur = heure ; 
   var formatDeMinute = minute < 10 ? "0" + minute : minute ; 
